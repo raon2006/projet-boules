44d4b8d351945bf12c48a328f80b3b3c42fb2122
Raphaël LEON <rleon@resoedu.talma>
2023-11-14 16:26:53 +0100

    Update projet boule.py

--- projet boule.py
+++ projet boule.py
@@ -32,23 +32,60 @@ couleur = (90,255,255)
 couleur4 = (244, 189, 3)
 couleur3 = (89,233,12)
 
+class balle:
+    def __init__(self,x,y,dx,dy):
+        self.x = x
+        self.dx = dx
+        self.y = y
+        self.dy = dy 
+    def draw(self):
+        pygame.draw.circle(fenetre,couleur,(self.x,self.y),10)
+    def move(self):
+        self.x +=  self.dx
+        self.y +=  self.dy
+    def paroi(self):
+        if self.x < 10:
+            self.dx *= (-1)
+        elif self.x > 630:
+            self.dx *= (-1)
+        elif self.y < 10:
+            self.dy *= (-1)
+        elif self.y > 470:
+            self.dy *= (-1)
+
+balle1 =balle(56,75,2,5)
+liste = [balle1]
+
+
 def collision(n):
     if x == x2 or y == y2:
         dx = dx(-1)
         dx2 = dx2(-1)
         print("collision")
         collision(9)
+
+
+      
+
+
 while True :
     fenetre.fill([0,0,0])
     pygame.draw.circle(fenetre,couleur,(x,y),10)
     pygame.draw.circle(fenetre,couleur3,(x2,y2),5) #normalement ici c'est couleur2 et pas couleur3
     pygame.draw.circle(fenetre,couleur4,(x3,y3),15)
+    balle1.draw()
+    balle1.move()
+    balle1.paroi()
     x += dx
     y += dy
     x2 += dx2
     y2 += dy2
     x3 += dx3
     y3 += dy3
+
+    for b in liste:
+        b.draw()
+        b.move()
 
     if x < 10:
         dx *= (-1)
@@ -79,7 +116,7 @@ while True :
         dy2 *= (-1)
 
     
-    
+
 
 
     pygame.display.update()
